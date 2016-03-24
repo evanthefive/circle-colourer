182550ad0a631d31d2001de707efce4e857e9167
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-23 20:41:36 -0400

    Markbot adjustments

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ var $ball = $('div');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-$picker.on ('change', function() {
+$picker.on('change', function () {
   console.log('works');
   $ball.css('background-color', $input.val());
-  });
+  $('.ball');
+});
