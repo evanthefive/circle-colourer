c646c03fb75e12aab40507edb61f8312e85f1a8b
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-23 20:38:34 -0400

    Market adjustments

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ var $ball = $('div');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-
 $picker.on('change', function() {
-  console.log('works');
-  $ball.css('background-color', $input.val());
-  });
+	console.log('works');
+	$ball.css('background-color', $input.val());
+	});
